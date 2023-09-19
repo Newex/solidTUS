bd7093bef69e7dfd83ff6a452a6c8c08a1bedad2
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2023-09-19 16:05:56 -0200

    test: Parsing logic

--- tests/ParserTests/MetadataParserTests/ParseInputToDictionary.cs
+++ tests/ParserTests/MetadataParserTests/ParseInputToDictionary.cs
@@ -45,4 +45,25 @@ public class ParseInputToDictionary
             second => Assert.Equal(string.Empty, second.Value),
             third => Assert.Equal(Value3, third.Value));
     }
+
+    [Fact]
+    public void Fast_parse_even_with_empty_value_in_the_middle_and_no_spaces()
+    {
+        // Arrange
+        const string Key1 = "Filename";
+        const string Value1 = "how_to.pdf";
+        const string Key2 = "Empty";
+        const string Key3 = "MimeType";
+        const string Value3 = "application/pdf";
+        var input = $"{Key1} {Base64Converters.Encode(Value1)},{Key2},{Key3} {Base64Converters.Encode(Value3)}";
+
+        // Act
+        var result = MetadataParser.ParseFast(input);
+
+        // Assert
+        Assert.Collection(result,
+            first => Assert.Equal(Value1, first.Value),
+            second => Assert.Equal(string.Empty, second.Value),
+            third => Assert.Equal(Value3, third.Value));
+    }
 }
