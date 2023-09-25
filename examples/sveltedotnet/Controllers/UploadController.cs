d1bec5f882ffc235f67909ebc266dff7ae967c48
Newex <mojo11ku@hotmail.com>
2023-09-25 18:23:20 -0200

    wip: Update example

--- examples/sveltedotnet/Controllers/UploadController.cs
+++ examples/sveltedotnet/Controllers/UploadController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using SolidTUS.Attributes;
 using SolidTUS.Contexts;
 using SolidTUS.Handlers;
+using SolidTUS.Models;
 
 namespace ViteDotnet.Controllers;
 
@@ -23,30 +24,6 @@ public class UploadController : ControllerBase
         this.uploadMetaHandler = uploadMetaHandler;
     }
 
-    [TusUpload("{fileId}")]
-    [RequestSizeLimit(5_000_000_000)]
-    public async Task<ActionResult> Upload(string fileId, [FromServices] TusUploadContext context)
-    {
-        // Do not await if you want the callback
-
-        // Callback
-        context.OnUploadFinished(async (file) =>
-        {
-            var filename = file.Metadata["name"];
-            Console.WriteLine($"Uploaded file {filename} with file size {file.FileSize}");
-            await Task.CompletedTask;
-        });
-
-        context.SetExpirationStrategy(SolidTUS.Models.ExpirationStrategy.SlidingExpiration, TimeSpan.FromSeconds(30));
-
-        // Await after callback defined
-        await context.StartAppendDataAsync(fileId);
-        // await upload;
-
-        // Must always return 204 on upload success with no Body content
-        return NoContent();
-    }
-
     [TusCreation("/api/upload")]
     public async Task<ActionResult> CreateFile([FromServices] TusCreationContext context)
     {
@@ -54,29 +31,38 @@ public class UploadController : ControllerBase
         var filename = context.UploadFileInfo.Metadata["name"];
         var mime = context.UploadFileInfo.Metadata["type"];
 
-        // Construct upload URL
+        // Construct some unique file id
         var id = Guid.NewGuid().ToString("N");
-        // var uploadTo = Url.Action(nameof(Upload), new { fileId = id }) ?? string.Empty;
 
-        // Can define callback before starting upload (creation-with-upload)
-        context.OnUploadFinished(async () =>
-        {
-            // only if the upload HAS data will this be called
-            Console.WriteLine("Finished uploading this file: " + filename);
-            await Task.CompletedTask;
-        });
-
-        // Start creation (IuploadStorageHandler.CreateResource())
+        // Accept creating upload and redirect to TusUpload
         await context.StartCreationAsync(id, routeValues: new { fileId = id });
-
 
         // Converts a success to 201 created
         return Ok();
     }
 
-    // Must have same route as the Upload route
+    [TusUpload("{fileId}")]
+    [RequestSizeLimit(5_000_000_000)]
+    public async Task<ActionResult> UploadFile(string fileId, [FromServices] TusUploadContext context)
+    {
+        // Set callback before awaiting upload, otherwise the callback won't be called
+        context.OnUploadFinished(async (file) =>
+        {
+            var filename = file.Metadata["name"];
+            Console.WriteLine($"Uploaded file {filename} with file size {file.FileSize}");
+            await Task.CompletedTask;
+        });
+
+        context.SetExpirationStrategy(ExpirationStrategy.SlidingExpiration, TimeSpan.FromSeconds(30));
+        await context.StartAppendDataAsync(fileId);
+
+        // Must always return 204 on upload success with no Body content
+        return NoContent();
+    }
+
+    // MUST have same route as the Upload route
     [TusDelete("{fileId}")]
-    public async Task<ActionResult> DeleteUpload(string fileId, CancellationToken cancellationToken)
+    public async Task<ActionResult> DeleteFile(string fileId, CancellationToken cancellationToken)
     {
         // No questions asked - just delete
         var info = await uploadMetaHandler.GetResourceAsync(fileId, cancellationToken);
