07474e2d4109ed5e7fa9ae75138ff52d7c8fdb8f
Newex <mojo11ku@hotmail.com>
2023-09-28 07:35:25 -0200

    test: Route template parser

---
+++ tests/ParserTests/RouteTemplateParserTests/RouteTemplateParserTests.cs
@@ -0,0 +1,22 @@
+using SolidTUS.ProtocolHandlers.ProtocolExtensions;
+
+namespace SolidTUS.Tests.ParserTests.RouteTemplateParserTests;
+
+public class RouteTemplateParserTests
+{
+    [Theory]
+    [InlineData("/api/route/{fileId}", "fileId", "/api/route/myFileA", "myFileA")]
+    [InlineData("/api/route/{fileId:int}", "fileId", "/api/route/123", "123")]
+    [InlineData("/api/{partialId:alpha}", "partialId", "/api/my-partial-id", "my-partial-id")]
+    [InlineData("api/{partialId:alpha}", "partialId", "area/controller/api/my-partial-id", "my-partial-id")]
+    [InlineData("api/some-route-prefix-{partialId:alpha}", "partialId", "area/controller/api/some-route-prefix-my-partial-id", "my-partial-id")]
+    [InlineData("/api/route/{fileId}", "fileId", "/api/route", null)]
+    public void Given_the_template_and_token_should_return_the_token_value_for_template(string template, string token, string input, string value)
+    {
+        // Act
+        var result = ConcatenationRequestHandler.GetTemplateValue(input, template, token);
+
+        // Assert
+        result.Should().Be(value);
+    }
+}
