c6ff9d1952ddfc68ebdaa832007a757e707875a8
Newex <mojo11ku@hotmail.com>
2023-10-04 05:27:08 -0200

    docs: Add xml documentation

--- src/Contexts/ResponseContext.cs
+++ src/Contexts/ResponseContext.cs
@@ -1,24 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Http;
-
 using SolidTUS.Models;
 
 namespace SolidTUS.Contexts;
 
+/// <summary>
+/// The response context
+/// </summary>
 public record ResponseContext
 {
+    /// <summary>
+    /// Instantiate a new object <see cref="ResponseContext"/>
+    /// </summary>
+    /// <param name="responseHeaders">The response headers</param>
     public ResponseContext(IHeaderDictionary responseHeaders)
     {
         ResponseHeaders = responseHeaders;
     }
 
+    /// <summary>
+    /// Get the response headers
+    /// </summary>
     public IHeaderDictionary ResponseHeaders { get; }
 
+    /// <summary>
+    /// Get the related upload file info
+    /// </summary>
     public UploadFileInfo? UploadFileInfo { get; internal set; }
 
+    /// <summary>
+    /// Get the created location url
+    /// </summary>
     public string? LocationUrl { get; internal set; }
 }
