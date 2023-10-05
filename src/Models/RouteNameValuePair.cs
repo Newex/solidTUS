9dcb6a95b462fe54fd33531658854ca016f2c0c6
Newex <mojo11ku@hotmail.com>
2023-10-05 20:11:34 -0200

    wip: Delete unused class

--- src/Models/RouteNameValuePair.cs
+++
@@ -1,8 +0,0 @@
-namespace SolidTUS.Models;
-
-/// <summary>
-/// Route data containing info about the route name and the route values
-/// </summary>
-/// <param name="RouteName">The route name</param>
-/// <param name="RouteValues">The route values</param>
-public record struct RouteNameValuePair(string? RouteName, object? RouteValues);
